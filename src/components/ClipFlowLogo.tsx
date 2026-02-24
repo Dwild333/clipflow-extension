7d3c86ab062c29715f7af7bad276493797a7c63d
Dwild333 <168043306+Dwild333@users.noreply.github.com>
2026-02-24 10:10:05 +0700

    feat: use custom icon.png in ClipFlowLogo component, add PNG type declaration

--- src/components/ClipFlowLogo.tsx
+++ src/components/ClipFlowLogo.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react'
+import icon from '../../assets/icon.png'
 
 interface ClipFlowLogoProps {
   size?: number
@@ -6,34 +6,13 @@ interface ClipFlowLogoProps {
 }
 
 export function ClipFlowLogo({ size = 28, className = '' }: ClipFlowLogoProps) {
-  const uid = useId()
-  const gradId = `c2n-grad-${uid}`
-
   return (
-    <svg
+    <img
+      src={icon}
       width={size}
       height={size}
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={`inline-block shrink-0 ${className}`}
-    >
-      <rect width="32" height="32" rx="8" fill={`url(#${gradId})`} />
-      <rect x="4.5" y="5" width="15" height="18" rx="3" fill="white" opacity="0.3" />
-      <rect x="12.5" y="9" width="15" height="18" rx="3" fill="white" />
-      <path
-        d="M16.5 18L19.5 21L24 15"
-        stroke="#6366F1"
-        strokeWidth="2.2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <defs>
-        <linearGradient id={gradId} x1="0" y1="0" x2="32" y2="32" gradientUnits="userSpaceOnUse">
-          <stop offset="0%" stopColor="#6366F1" />
-          <stop offset="100%" stopColor="#8B5CF6" />
-        </linearGradient>
-      </defs>
-    </svg>
+      alt="ClipFlow"
+      className={`inline-block shrink-0 rounded-lg ${className}`}
+    />
   )
 }
